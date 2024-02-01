f9b19312daef51a28e27bf17fb3a2c152e040ed7
devdamirbek@gmail.com <developerdamirbek@mail.ru>
2024-02-01 17:38:15 +0500

    new changes

---
+++ src/assets/icons/location-icon.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const LocationIcon = () => {
+  return (
+<svg width={32.000000} height="38.109131" viewBox="0 0 32 38.1091" fill="none" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+  <desc>
+    Created with Pixso.
+  </desc>
+  <defs />
+  <path id="Vector" d="M16 37C16 37 31 29.8 31 15.3999C31 7.44995 24.2876 1 16 1C7.7124 1 1 7.44995 1 15.3999C1 29.8 16 37 16 37Z" stroke="#1F2B6C" strokeOpacity={1.000000} strokeWidth={2.000000} />
+  <path id="Vector" d="M16 20.3201C13.1504 20.3201 10.8401 18.0098 10.8401 15.1599C10.8401 12.3103 13.1504 10 16 10C18.8499 10 21.1602 12.3103 21.1602 15.1599C21.1602 18.0098 18.8499 20.3201 16 20.3201Z" stroke="#159EEC" strokeOpacity={1.000000} strokeWidth={2.000000} />
+</svg>
+
+
+  )
+}
---
+++ src/layout/header/data.js
@@ -0,0 +1,26 @@
+export const links = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "About Us",
+    path: "/about",
+  },
+  {
+    name: "Services",
+    path: "/services"
+  },
+  {
+    name: "Doctors",
+    path: "/doctors",
+  },
+  {
+    name: "News",
+    path: "/news",
+  },
+  {
+    name: "Contact",
+    path: "/contact",
+  }
+];
