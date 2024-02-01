14f8ae66180da620ea5c2b844d654e2931e8ff87
Xondamir <abdulazizovxondamirxon@gmail.com>
2024-02-01 23:51:30 +0500

    header 85% tugatildi

---
+++ src/assets/icons/search-icon.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function SearchIcon() {
+  return (
+    <svg width="23" height="22" viewBox="0 0 23 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M10.3889 18.7779C15.2982 18.7779 19.2779 14.7982 19.2779 9.88894C19.2779 4.97971 15.2982 1 10.3889 1C5.47971 1 1.5 4.97971 1.5 9.88894C1.5 14.7982 5.47971 18.7779 10.3889 18.7779Z" stroke="#FCFEFE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M21.5002 21.0001L16.6668 16.1667" stroke="#FCFEFE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+  )
+}
+
+export default SearchIcon
--- src/layout/header/header.jsx
+++ src/layout/header/header.jsx
@@ -1,23 +1,61 @@
 import React from "react";
 import { links } from "./data";
 import { NavLink } from "react-router-dom";
-import style from './header.module.scss'
-
+import style from "./header.module.scss";
+import SearchIcon from "../../assets/icons/search-icon";
+import HeaderLogo from "../header/images/header-logo.svg";
+// import  Phoneicon from './../../assets/icons/phone-icon';
 export const Header = () => {
   return (
     <header>
-      <div className="container header_container">
-
-        <ul className={style.navbar}>
-          {links.map((item) => (
-            <li key={item.path}>
-              <NavLink className={style.navbar_link} to={item.path}>
-              {item.name}
-            </NavLink>
-            </li>
-          ))}
-        </ul>
+      <nav className={style.nav_flex}>
+        <img src={HeaderLogo} alt="" />
+        <div className={style.contacts}>
+          <div className={style.contact}>
+            {/* <img src={Phoneicon} alt="" /> */}
+            <span>img</span>
+            <div>
+              <p>Emergency</p>
+              <span>(237) 681-812-255</span>
+            </div>
+          </div>
+          <div className={style.contact}>
+            {/* <img src={Phoneicon} alt="" /> */}
+            <span>img</span>
+            <div>
+              <p>Work Hour</p>
+              <span>09:00 - 20:00 Everyday</span>
+            </div>
+          </div>
+          <div className={style.contact}>
+            {/* <img src={Phoneicon} alt="" /> */}
+            <span>img</span>
+            <div>
+              <p>Location</p>
+              <span>0123 Some Place</span>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div className="container header_container ">
+        <div className={style.navbar}>
+          <ul className={style.nav}>
+            {links.map((item) => (
+              <li className={style.header_Li} key={item.path}>
+                <NavLink className={style.navbar_link} to={item.path}>
+                  {item.name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+          <div className={style.search_btn}>
+            <SearchIcon />
+            <button className={style.header_btn}>Appointment</button>
+          </div>
+        </div>
       </div>
+
+    
     </header>
   );
 };
