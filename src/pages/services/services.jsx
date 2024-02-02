88ccc572f40f2c6461eaa709dae1991443aa975c
Bahriddin Shavqiyev <shavqiyevb@gmail.com>
2024-02-02 14:16:40 +0500

    salom

--- src/pages/services/services.jsx
+++ src/pages/services/services.jsx
@@ -1,7 +1,90 @@
-import React from 'react'
+// import React from 'react'
+import style from "./services.module.scss"
+import main_img from "./images/main_imgs.svg"
+import free from "./images/free.jpg"
+
+import icons from "./images/icons.svg"
+
 
 export const Services = () => {
   return (
-    <div>services</div>
+   <div>
+    <div className={style.main}>
+    <div className="">
+      <div className={style.style_main}>
+      <img src={main_img} alt="imgs" />
+       <div className='container'>
+       <div className={style.style_title}>
+        <p>Home / Services</p>
+        <h1>Our Services</h1>
+       </div>
+       </div>
+      </div>
+    </div>
+   </div>
+
+   <div className={style.Free}>
+    <div className='container'>
+      <div className={style.card_img} >
+         <img className={style.style_img} src={free} alt="" />
+         <img src={icons} className={style.icons} alt="" />
+         <div className={style.style_checkup}>
+          <h2>Free Checkup</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing Quisque placerat Convallis felis vitae tortor augue. Velit nascetur massa in.</p>
+         <button>Learn More </button>
+         </div>
+      </div>
+      <div className={style.card_img} >
+         <img className={style.style_img} src={free} alt="" />
+         <img src={icons} className={style.icons} alt="" />
+         <div className={style.style_checkup}>
+          <h2>Free Checkup</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing Quisque placerat Convallis felis vitae tortor augue. Velit nascetur massa in.</p>
+         <button>Learn More </button>
+         </div>
+      </div>
+      <div className={style.card_img} >
+         <img className={style.style_img} src={free} alt="" />
+         <img src={icons} className={style.icons} alt="" />
+         <div className={style.style_checkup}>
+          <h2>Free Checkup</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing Quisque placerat Convallis felis vitae tortor augue. Velit nascetur massa in.</p>
+         <button>Learn More </button>
+         </div>
+      </div>
+      <div className={style.card_img} >
+         <img className={style.style_img} src={free} alt="" />
+         <img src={icons} className={style.icons} alt="" />
+         <div className={style.style_checkup}>
+          <h2>Free Checkup</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing Quisque placerat Convallis felis vitae tortor augue. Velit nascetur massa in.</p>
+         <button>Learn More </button>
+         </div>
+      </div>
+      <div className={style.card_img} >
+         <img className={style.style_img} src={free} alt="" />
+         <img src={icons} className={style.icons} alt="" />
+         <div className={style.style_checkup}>
+          <h2>Free Checkup</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing Quisque placerat Convallis felis vitae tortor augue. Velit nascetur massa in.</p>
+         <button>Learn More </button>
+         </div>
+      </div>
+      <div className={style.card_img} >
+         <img className={style.style_img} src={free} alt="" />
+         <img src={icons} className={style.icons} alt="" />
+         <div className={style.style_checkup}>
+          <h2>Free Checkup</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing Quisque placerat Convallis felis vitae tortor augue. Velit nascetur massa in.</p>
+         <button>Learn More </button>
+         </div>
+      </div>
+    </div>
+
+   </div>
+
+
+   </div>
+   
   )
 }
