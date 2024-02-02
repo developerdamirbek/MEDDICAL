cfa031de7f239421f6a7dbd95fc6e85c7474f36b
devdamirbek@gmail.com <developerdamirbek@mail.ru>
2024-02-02 17:11:08 +0500

    change by damirbek

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -5,6 +5,12 @@ import { ContactCards } from '../../components/contactcards'
 import { ServiceCard } from '../../components/servicecard'
 import { Service } from '../../components/services/service'
 import { RightArrow } from '../../assets/icons/right-arrow'
+import { CheckupIcon } from '../../assets/icons/checkup-icon'
+import { HeartPlus } from '../../assets/icons/heart-plus'
+import { SquarePlus } from '../../assets/icons/square-plus'
+import { BloodBank } from '../../assets/icons/blood-bank'
+import img1 from '../../assets/images/ser1.png'
+import img2 from '../../assets/images/ser2.png'
 
 export const Home = () => {
     return (
@@ -49,7 +55,52 @@ export const Home = () => {
                     <h2 className={style.home_services__title}>Our Services</h2>
                     <div className={style.home_services__content}>
                         <div className={style.home_services__tab}>
-
+                            <div className={style.service_tab}>
+                                <div className={style.tab_icon}>
+                                    <CheckupIcon/>
+                                </div>
+                                <h3 className={style.tab_title}>Free Checkup</h3>
+                            </div>
+                            <div className={style.service_tab}>
+                                <div className={style.tab_icon}>
+                                    <HeartPlus/>
+                                </div>
+                                <h3 className={style.tab_title}>Cardiogram</h3>
+                            </div>
+                            <div className={style.service_tab}>
+                                <div className={style.tab_icon}>
+                                    <SquarePlus/>
+                                </div>
+                                <h3 className={style.tab_title}>Dna Testing</h3>
+                            </div>
+                            <div className={style.service_tab}>
+                                <div className={style.tab_icon}>
+                                    <BloodBank/>
+                                </div>
+                                <h3 className={style.tab_title}>Blood Bank</h3>
+                            </div>
+                            <Link to="/services">
+                                <button className={style.tab_btn}>View All</button>
+                            </Link>
+                        </div>
+                        <div className={style.home_services__info}>
+                            <h2 className={style.services_info__title}>A passion for putting patients first.</h2>
+                            <div>
+                                <ul>
+                                    <li>A Passion for Healing</li>
+                                    <li>All our best</li>
+                                    <li>A Legacy of Excellence</li>
+                                </ul>
+                                <ul>
+                                    <li>5-Star Care</li>
+                                    <li>Believe in Us</li>
+                                    <li>Always Caring</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className={style.home_services__images}>
+                            <img src={img1} alt="img" />
+                            <img src={img2} alt="img" />
                         </div>
                     </div>
                 </div>
